a71979c42261a0240d30b5275c3b41885b2048f4
Elvis Inoa <einoa04@gmail.com>
2023-01-18 00:58:31 -0400

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+import useClima from '../hooks/useClima';
+
+export default function Formulario() {
+
+  const [alerta, setAlerta] = useState('');
+
+  const { busqueda, datosBusqueda, consultarClima } = useClima();
+
+  const { ciudad, pais } = busqueda;
+
+  const handleSubmit = e => 
+  {
+    e.preventDefault();
+
+    if(Object.values(busqueda).includes(''))
+    {
+      setAlerta('Todos los campos son obligatorios.');
+      return;
+    }
+
+    setAlerta('');
+    consultarClima(busqueda);
+  }
+
+  return (
+    <div className="contenedor">
+
+      {alerta && <p>{alerta}</p>}
+      <form
+        onSubmit={handleSubmit}
+      >
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input 
+            type="text" 
+            id="ciudad"
+            name="ciudad"
+            onChange={datosBusqueda}
+            value={ciudad}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="pais">Pais</label>
+          <select 
+            name="pais" 
+            id="pais"
+            onChange={datosBusqueda}
+            value={pais}
+          >
+            <option value="">-- Seleccione un país --</option>
+            <option value="US">Estados Unidos</option>
+            <option value="DO">Republica Dominicana</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Consultar Clima" />
+      </form>
+    </div>
+  )
+}
---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  )
+}
